a60eeef935a1e9e51b657a2b5a46a7829d3b843b
dabzr <dylanab@pm.me>
2025-12-11 21:38:40 -0300

    adaptações hehe

--- model.py
+++ model.py
@@ -2,9 +2,8 @@ import torch
 import torch.nn as nn
 import math
 
-# ATTENTION IS ALL YOU NEED 
-
 class InputEmbeddings(nn.Module):
+
     def __init__(self, d_model: int, vocab_size: int) -> None:
         super().__init__()
         self.d_model = d_model
@@ -14,81 +13,97 @@ class InputEmbeddings(nn.Module):
     def forward(self, x):
         return self.embedding(x) * math.sqrt(self.d_model)
 
+
 class PositionalEncoding(nn.Module):
+    """
+    Para grafos, esta parte será substituída por RWPE, Laplacian PE etc.
+    """
     def __init__(self, d_model: int, seq_len: int, dropout: float) -> None:
         super().__init__()
-        self.d_model = d_model
-        self.seq_len = seq_len
         self.dropout = nn.Dropout(dropout)
 
         pe = torch.zeros(seq_len, d_model)
         position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0)/ d_model))
-        pe[:, 0::2] = torch.sin(position * div_term) # PE[i,2k] = sin(i / 10000^(2k/d_model))
-        pe[:, 1::2] = torch.cos(position * div_term) # PE[i,2k+1] = cos(i / 10000^(2k/d_model))
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() *
+                             (-math.log(10000.0) / d_model))
+
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
 
         self.pe = pe.unsqueeze(0)
-        self.register_buffer('pe', pe)
+        self.register_buffer("pe", pe)
+
     def forward(self, x):
-        x = x + (self.pe[:, :x.shape[1], :]).requires_grad_(False)
+        x = x + self.pe[:, :x.size(1)]
         return self.dropout(x)
 
-class LayerNormalization(nn.Module): # equivalente ao nn.LayerNorm(d_model, eps=eps)
-    def __init__(self, d_model, eps: float = 10**-6) -> None:
-        super().__init__()
-        self.eps = eps
-        self.alpha = nn.Parameter(torch.ones(d_model))
-        self.bias  = nn.Parameter(torch.zeros(d_model))
-
-    def forward(self, x):
-        mean = x.mean(dim = -1, keepdim=True)
-        std = x.std(dim = -1, keepdim=True)
-        return self.alpha * (x-mean) / (std+self.eps) + self.bias # EPS evita divisão por zero
-
-class FeedForwardBlock(nn.Module):
-    def __init__(self, d_model:int, d_ff:int, dropout:float) -> None:
-        super().__init__()
-        self.linear_1 = nn.Linear(d_model, d_ff)
-        self.dropout = nn.Dropout(dropout)
-        self.linear_2 = nn.Linear(d_ff, d_model)
-    def forward(self, x):
-        return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
 
 class MultiHeadAttentionBlock(nn.Module):
+    """
+    ESTA É A PARTE QUE TERÁ QUE SER ADAPTADA PARA GRAFOS.
+    DOT-PRODUCT ATENÇÃO NÃO FUNCIONA EM GRAFOS SEM MÁSCARA DE ADJACÊNCIA.
+    """
     def __init__(self, d_model:int, h:int, dropout:float) -> None:
         super().__init__()
         self.d_model = d_model
         self.h = h
-        assert d_model % h == 0, "d_model is not divisible by h"
+        assert d_model % h == 0
         self.d_k = d_model // h
+
         self.w_q = nn.Linear(d_model, d_model)
         self.w_k = nn.Linear(d_model, d_model)
         self.w_v = nn.Linear(d_model, d_model)
         self.w_o = nn.Linear(d_model, d_model)
-        self.dropout = nn.Dropout(dropout)
-    @staticmethod
-    def attention(query, key, value, mask, dropout: nn.Dropout):
-        d_k = query.shape[-1]
 
-        attention_scores = (query * key.transpose(-2, -1)) / math.sqrt(d_k)
+        self.dropout = nn.Dropout(dropout)
+
+    @staticmethod
+    def attention(query, key, value, mask, dropout):
+        d_k = query.size(-1)
+
+        attention_scores = (query @ key.transpose(-2, -1)) / math.sqrt(d_k)
+
         if mask is not None:
-            attention_scores.masked_fill_(mask == 0, -1e9) # -1e9 é um valor mto pequeno, equivalente aqui ao -inf
+            attention_scores = attention_scores.masked_fill(mask == 0, -1e9)
+
         attention_scores = attention_scores.softmax(dim=-1)
+
         if dropout is not None:
             attention_scores = dropout(attention_scores)
-        return (attention_scores @ value), attention_scores
+
+        return attention_scores @ value, attention_scores
 
     def forward(self, q, k, v, mask):
-        query = self.w_q(q)
-        key = self.w_k(k)
+        B, L, _ = q.size()
+
+        query = self.w_q(q)  # [B,L,d_model]
+        key   = self.w_k(k)
         value = self.w_v(v)
-        
-        query = query.view(query.shape[0], query.shape[1], self.h, self.d_k).transpose(1, 2)
-        key = key.view(key.shape[0], key.shape[1], self.h, self.d_k).transpose(1, 2)
-        value = value.view(value.shape[0], value.shape[1], self.h, self.d_k).transpose(1, 2)
-        x, self.attention_scores = MultiHeadAttentionBlock.attention(query, key, value, mask, self.dropout)
-        
-        x = x.transpose(1, 2).contiguous().view(x.shape[0], -1, self.h * self.d_k)
+
+        # reshape em multi-heads
+        query = query.reshape(B, L, self.h, self.d_k).transpose(1, 2)
+        key   = key.reshape(B, L, self.h, self.d_k).transpose(1, 2)
+        value = value.reshape(B, L, self.h, self.d_k).transpose(1, 2)
+
+        x, attn = MultiHeadAttentionBlock.attention(query, key, value, mask, self.dropout)
+
+        x = x.transpose(1, 2).reshape(B, L, self.h * self.d_k)
+
         return self.w_o(x)
 
+
+
+# Layer and Norm:
+# nn.LayerNorm(d_model)
+
+# Feed Forward Network: 
+# nn.Sequential(
+#     nn.Linear(d_model, d_ff),
+#     nn.ReLU(),
+#     nn.Dropout(dropout),
+#     nn.Linear(d_ff, d_model)
+# )
+
+# Encoder & Decoder:
+# nn.TransformerEncoderLayer e nn.TransformerEncoder
 
